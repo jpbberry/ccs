caf6b80f89b618acb0dcdaa8ff765a48f026511b
JPBBerry <jb@jt3ch.net>
2019-11-07 18:57:14 -0500

    getToken

--- config.js
+++ config.js
@@ -4,7 +4,8 @@ const config = {
     host: 'localhost',
     port: '27017',
     db: 'ccs'
-  }
+  },
+  ...require('./private-config.js')
 }
 
 module.exports = config
--- functions.js
+++ functions.js
@@ -1,12 +1,24 @@
+const redirectURL = 'https://api.jt3ch.net/ccs/auth/callback'
+
 const fetch = require('node-fetch')
+function encodeJSON (element, key, list) {
+  list = list || []
+  if (typeof (element) === 'object') {
+    for (const idx in element) { encodeJSON(element[idx], key ? key + '[' + idx + ']' : idx, list) }
+  } else {
+    list.push(key + '=' + encodeURIComponent(element))
+  }
+  return list.join('&')
+}
 module.exports = (client) => {
-  client.request = async (url, method = 'GET', body, token) => {
+  client.request = async (url, method = 'GET', body, token, headers) => {
     return new Promise((resolve, reject) => {
       fetch(`https://discordapp.com/api/v7${url}`, {
         method: method,
         headers: {
           'Content-Type': 'application/json',
-          Authorization: token
+          Authorization: token,
+          ...headers
         },
         body: body ? JSON.stringify(body) : null
       })
@@ -18,5 +30,26 @@ module.exports = (client) => {
             })
         })
     })
+  }
+
+  client.getToken = async (code, refresh = false) => {
+    const f = await fetch('https://discordapp.com/api/v7/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: encodeJSON(
+        {
+          client_id: client.config.oauth.id,
+          client_secret: client.config.oauth.secret,
+          code: refresh ? undefined : code,
+          refresh_token: refresh ? code : undefined,
+          grant_type: refresh ? 'refresh_token' : 'authorization_code',
+          redirect_uri: redirectURL,
+          scope: 'identify guilds'
+        }
+      )
+    })
+    return f.json()
   }
 }
