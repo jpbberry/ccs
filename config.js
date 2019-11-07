eeebeb552351c8b71b6a407ab2678b770e9a2926
JPBBerry <jb@jt3ch.net>
2019-11-07 17:37:00 -0500

    base

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+  port: 8123
+}
+
+module.exports = config
---
+++ index.js
@@ -0,0 +1,17 @@
+const client = {}
+client.config = require('./config.js')
+
+const express = require('express')
+let app = express()
+
+const fs = require('fs')
+const path = require('path')
+
+let routes = fs.readdirSync('./routes')
+routes.forEach(route => {
+  if (!route.endsWith('.js')) return
+  const routeFile = require(path.resolve('./routes', route))
+  app.use(`./${route.split('.')[0]}`, routeFile(client))
+})
+
+app.listen(client.config.port)
